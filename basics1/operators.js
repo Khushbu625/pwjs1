93a66e5b8f4cab330728244315b47d93d3c54902
Khushbu625 <148690702+Khushbu625@users.noreply.github.com>
2024-03-30 06:30:07 +0000

    Discussion on operators

---
+++ basics1/operators.js
@@ -0,0 +1,34 @@
+/*Arithmetic operators
+ 5+5
+ 4-1
+ 6*2
+ 10/5
+ 13%2
+*/
+ const numberone = 2
+  const numbertwo = 3
+  
+  const numberthree = numberone + numbertwo
+  
+ console.log(numberthree);
+
+//  2**3=>2*2*2
+//  let valueone=2
+//    valueone++
+//    valueone=valueone+1
+//    ++valueone
+//    assignment operators
+ 
+let valuetwo = 2
+   valuetwo += 10
+   console.log(valuetwo);
+// comparison operators
+ let pw = 3
+ let pwskills = "3"
+ 
+ console.log(pw==pwskills);
+ console.log(pw!=pwskills);
+ console.log(pw===pwskills);
+
+
+
