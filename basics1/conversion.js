8deb0e8ce832ebfc414f543103c1de154c0840ca
Khushbu625 <148690702+Khushbu625@users.noreply.github.com>
2024-03-31 10:35:52 +0000

    Discussion on conversion

---
+++ basics1/conversion.js
@@ -0,0 +1,9 @@
+let bankbalance = 'abc'
+let bankbalanceinInt = Boolean(bankbalance)
+
+console.log(typeof bankbalance);
+console.log(typeof bankbalanceinInt);
+
+console.log(5+undefined);
+console.log(5+null);
+
