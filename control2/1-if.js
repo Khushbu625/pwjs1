ba0d61b61b04c59ac2cccebfab91da3809d3f0c9
Khushbu625 <148690702+Khushbu625@users.noreply.github.com>
2024-04-22 05:22:43 +0000

    short hand if else

--- control2/1-if.js
+++ control2/1-if.js
@@ -23,3 +23,29 @@ if(temperature<20){
     console.log("you lost a life");
 }
 console.log(`your score is ${score} and your life is ${life} and your bullets is ${bullets}` );
+//  we can write it as below also
+let balance = 500
+ if (balance>100) 
+  console.log("balance is above 100 ");
+
+let getStarFromUser = "5"
+let starInNumber = parseInt(getStarFromUser)
+
+if (starInNumber==1){
+ console.log("poor");
+}else if(starInNumber==2){
+console.log("ok");
+}else if(starInNumber==3){
+console.log("average");
+}else if(starInNumber==4){
+console.log("cool")
+}else if(starInNumber==5){
+console.log("great");
+}else if(starInNumber==6){
+console.log("awesome");
+}else if(starInNumber==7){
+console.log("nailed it");
+}
+else {
+    console.log("i don't know the rating");
+}
