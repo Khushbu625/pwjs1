1bf4e2d26779064c5ff1a2ac0ea297ee3d729aff
Khushbu625 <148690702+Khushbu625@users.noreply.github.com>
2024-04-23 10:18:49 +0000

    discussion on for-break-continue

---
+++ control2/2-switch.js
@@ -0,0 +1,31 @@
+
+let getStarFromUser = "5"
+let starInNumber = parseInt(getStarFromUser)
+
+switch(starInNumber){
+    case 1:
+        console.log("poor");
+        break;
+    case 2:
+        console.log("ok");
+        break;
+    case 3:
+        console.log("average");
+         break;
+    case 4:
+        console.log("cool");
+         break;
+    case 5:
+         console.log("great");
+         break;
+    case 6:
+         console.log("awesome");
+         break;
+    case 7:
+         console.log("nailed it");
+         break;
+    default:
+         console.log("i dont know the rating");
+}
+                                
+                                    
